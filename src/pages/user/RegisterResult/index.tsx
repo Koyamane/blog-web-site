36d0103263977bb55bdee6e38ccbb906a30f1179
koyamane <koyamane@local.pc>
2022-01-07 19:17:32 +0800

    style: 注册结果页面适配移动端

--- src/pages/user/RegisterResult/index.tsx
+++ src/pages/user/RegisterResult/index.tsx
@@ -1,48 +1,20 @@
 import React from 'react'
 import { Button, Result } from 'antd'
 import { FormattedMessage, Link, useModel } from 'umi'
-
 import styles from './style.less'
-
-const actions = (
-  <div className={styles.actions}>
-    <Link to='/'>
-      <Button size='large'>
-        <FormattedMessage id='pages.home.backHome' defaultMessage='回首页' />
-      </Button>
-    </Link>
-    <Link to='/account/settings'>
-      <Button size='large' type='primary'>
-        <FormattedMessage
-          id='pages.register.result.improvePersonalInformation'
-          defaultMessage='完善个人信息'
-        />
-      </Button>
-    </Link>
-  </div>
-)
 
 const RegisterResult: React.FC = () => {
   const { initialState } = useModel('@@initialState')
 
   return (
     <Result
-      className={styles.registerResult}
       status='success'
       title={
-        <div className={styles.title}>
-          <span>
-            <FormattedMessage
-              id='pages.register.result.success.prefix'
-              defaultMessage='你的账号：'
-            />
-            {initialState?.currentUser?.username}
-            <FormattedMessage
-              id='pages.register.result.success.suffix'
-              defaultMessage=' 注册成功'
-            />
-          </span>
-        </div>
+        <span>
+          <FormattedMessage id='pages.register.result.success.prefix' defaultMessage='你的账号：' />
+          {initialState?.currentUser?.username}
+          <FormattedMessage id='pages.register.result.success.suffix' defaultMessage=' 注册成功' />
+        </span>
       }
       subTitle={
         <FormattedMessage
@@ -50,7 +22,21 @@ const RegisterResult: React.FC = () => {
           defaultMessage='可以尽情地发布内容啦'
         />
       }
-      extra={actions}
+      extra={[
+        <Link key='home' to='/'>
+          <Button className={styles.btn}>
+            <FormattedMessage id='pages.home.backHome' defaultMessage='回首页' />
+          </Button>
+        </Link>,
+        <Link key='settings' to='/account/settings'>
+          <Button type='primary' className={styles.btn}>
+            <FormattedMessage
+              id='pages.register.result.improvePersonalInformation'
+              defaultMessage='完善个人信息'
+            />
+          </Button>
+        </Link>
+      ]}
     />
   )
 }
